f66f24a52404ee23ab103a300b2644045f6cc902
Alexander <a-l-e-k-s@yandex.ru>
2021-04-08 14:42:23 +0300

    Initial Commit

---
+++ HW3.5.ContactList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW3.5.ContactList
+//
+//  Created by Alexander Konovalov on 08.04.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HW3.5.ContactList/HW3_5_ContactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW3_5_ContactListApp.swift
+//  HW3.5.ContactList
+//
+//  Created by Alexander Konovalov on 08.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct HW3_5_ContactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
